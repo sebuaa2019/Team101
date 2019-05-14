1175471b22bc3982d9ecef19fed54acfe259c5c5
neolinsu <neo.jia.lin@outlook.com>
2019-05-14 10:20:58 +0800

    rm doc

--- Motor/Driver/demo.py
+++
@@ -1,8 +0,0 @@
-import motordriver as driver
-
-if __name__ == '__main__':
-    while True:
-        driver.t_up(100, 5)
-        driver.t_stop(1)
-        driver.t_right(100, 1)
-        driver.t_stop(1)
