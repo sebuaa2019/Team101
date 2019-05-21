d29b5ea454be3c9aef9ea3945e267f05041734a8
JamesDYX <jamesdyx@buaa.edu.cn>
2019-05-21 15:44:07 +0800

    Update UltrasonicMotor

--- src/Sensor/UltrasonicMotor.py
+++ src/Sensor/UltrasonicMotor.py
@@ -3,48 +3,51 @@
 import RPi.GPIO as GPIO
 import time
 
-# 定义 GPIO 引脚
-GPIO_TRIGGER = 20
-GPIO_ECHO = 21
 
-# 设置 GPIO 模式为 BCM
-GPIO.setmode(GPIO.BCM)
-# 设置 GPIO 的工作方式 (IN / OUT)
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-GPIO.setup(GPIO_ECHO, GPIO.IN)
+class UltrasonicMotor:
+    def __init__(self, GPIO_TRIGGER=20, GPIO_ECHO=21):
+        # 定义 GPIO 引脚
+        self.GPIO_TRIGGER = GPIO_TRIGGER
+        self.GPIO_ECHO = GPIO_ECHO
 
+        # 设置 GPIO 模式为 BCM
+        GPIO.setmode(GPIO.BCM)
+        # 设置 GPIO 的工作方式 (IN / OUT)
+        GPIO.setup(self.GPIO_TRIGGER, GPIO.OUT)
+        GPIO.setup(self.GPIO_ECHO, GPIO.IN)
 
-def distance():
-    # 发送高电平信号到 Trig 引脚
-    GPIO.output(GPIO_TRIGGER, True)
+    def distance(self):
+        # 发送高电平信号到 Trig 引脚
+        GPIO.output(self.GPIO_TRIGGER, True)
 
-    # 持续 10 us
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
+        # 持续 10 us
+        time.sleep(0.00001)
+        GPIO.output(self.GPIO_TRIGGER, False)
 
-    start_time = time.time()
-    stop_time = time.time()
-
-    # 记录发送超声波的时刻1
-    while GPIO.input(GPIO_ECHO) == 0:
         start_time = time.time()
-
-    # 记录接收到返回超声波的时刻2
-    while GPIO.input(GPIO_ECHO) == 1:
         stop_time = time.time()
 
-    # 计算超声波的往返时间 = 时刻2 - 时刻1
-    time_elapsed = stop_time - start_time
-    # 声波的速度为 343m/s， 转化为 34300cm/s。
-    dist = (time_elapsed * 343) / 2
+        # 记录发送超声波的时刻1
+        while GPIO.input(self.GPIO_ECHO) == 0:
+            start_time = time.time()
 
-    return dist
+        # 记录接收到返回超声波的时刻2
+        while GPIO.input(self.GPIO_ECHO) == 1:
+            stop_time = time.time()
+
+        # 计算超声波的往返时间 = 时刻2 - 时刻1
+        time_elapsed = stop_time - start_time
+        # 声波的速度为 343m/s， 转化为 34300cm/s。
+        dist = (time_elapsed * 343) / 2
+
+        return dist
 
 
 if __name__ == '__main__':
     try:
+        ultrasonic = UltrasonicMotor()
         while True:
-            dist = distance()
+            dist = ultrasonic.distance()
             print "Measured Distance = %.2f m" % dist
             # frequency: 5Hz
             time.sleep(0.2)
