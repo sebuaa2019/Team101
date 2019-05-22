68395bbd4dc8dd7ca7d55e6e0a7234fae7f21848
JamesDYX <jamesdyx@buaa.edu.cn>
2019-05-22 10:47:01 +0800

    修改了代码结构

---
+++ src/Motor/Controller.py
@@ -0,0 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from Sensor import GyroscopeDriver as Gyro
+from Sensor import UltrasonicDriver as UltraS
+
+if __name__ == '__main__':
+    pass
+
