09225c3c159fd01317a33acbc6bf7b66ea3d2cb7
16061089 <469265429@qq.com>
2019-06-09 21:35:21 +0800

    test

--- src/Visual/Visual_Analysis_testcase.py
+++ src/Visual/Visual_Analysis_testcase.py
@@ -1,10 +1,4 @@
-
-# coding: utf-8
-
-# In[33]:
-
-
-class BasePart:
+﻿class BasePart:
     
     def __init__(self,id,config):
         self.id=id
@@ -31,11 +25,21 @@ class Task_Analysis(BasePart):
     def task_identity(self,task):
         if(task.type=='classification'):
             return self.class_execution(task)
+        
+        else if(task.type=='growth'):
+            if(self.class_execution(task)!='plant'):
+                print('ERROR')
+            else:
+                return self.growth_execution(task)
+        
         else:
             return 'ERROR'
             
     def class_execution(self,task):
         return self.img_analysis.class_analyse(task)
+    
+    def growth_execution(self,task):
+        return self.img_analysis.growth_analyse(task)
 
         
 class Img_Analysis(BasePart):
@@ -47,18 +51,26 @@ class Img_Analysis(BasePart):
         
     def class_analyse(self,task):
         if(task.test==1):
-            return 'grass'
+            return task.img
         else:
             img = (np.expand_dims(task.img,0))
             prediction = self.modelpath.predict(img)
             return prediction
+        
+    def growth_analyse(self,task):
+        if(self.class_execution(task)!='plant'):
+            print('ERROR')
+        else:
+            return task.growth
 
 #testcase
 class Task():
     
-     def __init__(self,type,test):
+     def __init__(self,type,test,img,growth):
         self.type=type
         self.test=test
+        self.img=img
+        self.growth=growth
 
 #BasePart
 bp=BasePart(7,{'server':'hjk','client':'ljz'})
@@ -80,8 +92,3 @@ print('The type of plant is:',va.send_img_message(task),'\n')
                   
 task=Task('location',1)
 print('The type of plant is:',va.send_img_message(task))
-
-                  
-                  
-                  
-
